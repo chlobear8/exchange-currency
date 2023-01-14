77898c64558eef872ff8558e9f6fa46f935bbc93
Chloe ONeil <chloesporri@yahoo.com>
2023-01-13 18:06:47 -0800

    start async function for dropdown

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,15 +1,21 @@
 export default class CurrencyService {
-  static async getExchange(currency) {
+  static async getExchange(amount, targetCode) {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${targetCode}/${amount}`);
       const jsonifiedResponse = await response.json();
       if (!response.ok) {
       const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse.message}`;
         throw new Error(errorMessage);
       }
-      return jsonifiedResponse;
+      return jsonifiedResponse.conversion_result;
     } catch(error) {
       return error;
     }
+  }
+
+  static async fetchSupportedCurrencies() {
+    const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes`);
+    const newOption = document.getElementById('batchSelect');
+    newOption.value = 
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,42 @@
-// import "Object name" from "./projectname.js";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../css/styles.css';
+import CurrencyService from "./currency-service";
+
+//Business Logic
+
+async function getExchange(currency) {
+  CurrencyService.getExchange(currency)
+  const response = await CurrencyService.getExchange(currency);
+    if (response.main) {
+      printElements(response, currency);
+    } else {
+      printError(response, currency);
+    }
+}
+
+//UI Logic
+
+function printElements(reponse, currency) {
+  document.querySelector('#showResponse').innerText = `The currency exchange rate of ${currency} is ${response.conversion_rates} and the amount in US($) is ${currency * response.conversion_rates}.`;
+}
+
+function printError(error, currency) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the currency data for ${currency}: ${error}.`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const currency = document.querySelector('usDollar').value;
+  document.querySelector('#usDollar').value = null;
+  getExchange(currency);
+  
+  window.addEventListener("load", function() {
+    CurrencyService.fetchSupportedCurrencies().then(response => {
+      let dropdown = document.getElementById('batchSelect');
+      
+    })
+    document.querySelector('form').addEventListener("submit", handleFormSubmission);
+  });
+}
+
